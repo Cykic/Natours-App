4cc1fc3f2cafffff1ac1015506ffdd390b738ddd
cykic <cyb3rcykic@gmail.com>
2021-07-28 08:13:26 +0000

    For PR

--- src/tour/tourController.js
+++ src/tour/tourController.js
@@ -3,7 +3,7 @@ const catchAsync = require('../error/catchAsync');
 const factory = require('../handler/handlerFactory');
 const AppError = require('../error/appError');
 
-exports.aliasTopTours = (req, res, next) => {
+exports.aliasTopTours = (req, _res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
