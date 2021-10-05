6d30d454ad535d996b6ec669bd1cccc26019b4b4
cykic <cyb3rcykic@gmail.com>
2021-10-05 10:10:13 +0100

    Fix: Cookie fix

--- src/auth/authController.js
+++ src/auth/authController.js
@@ -94,7 +94,8 @@ exports.protected = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-
+  //  Cookie Fix
+  
   token = req.headers.cookie.split('=')[1];
   if (!token)
     return next(
